7f1537e1fcf8eb78c50f1146efaa5002fc9e60fd
EricDunbar <eric.dunbar@gmail.com>
2016-08-13 21:11:59 -0400

    A minor change

--- ODS/src/ods/DefaultComparator.java
+++ ODS/src/ods/DefaultComparator.java
@@ -5,7 +5,7 @@ package ods;
 
 import java.util.Comparator;
 
-class zzDefaultComparator<T> implements Comparator<T> {
+class DefaultComparator<T> implements Comparator<T> {
 	@SuppressWarnings("unchecked")
 	public int compare(T a, T b) {
 		return ((Comparable<T>)a).compareTo(b);
