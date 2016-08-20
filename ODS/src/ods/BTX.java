45849d4a4622923fb927135e7d9bd82d2285a7b3
Eric Dunbar <eric.dunbar@gmail.com>
2016-08-20 13:51:54 -0400

    Starting assignment 3, question 6

---
+++ ODS/src/ods/BTX.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package ods;
+
+/**
+ * An experiment to see if <Node> can work.
+ * 
+ * @author Eric Dunbar
+ * @param <T>
+ * @date Aug 16, 2016
+ * @title
+ * @assignment 2
+ *
+ */
+public class BTX<Node extends BTX.TheNode<T>, T> extends BinarySearchTree<BTX.TheNode<T>, T>{
+
+	/**
+	 * @param sampleNode
+	 */
+	public BTX(TheNode<T> sampleNode) {
+		super(sampleNode);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+		 * @author Eric Dunbar
+		 * @date Aug 16, 2016
+		 * @title
+		 * @assignment 2
+		 *
+		 */
+	public static class TheNode<T> {
+
+	}
+
+	/**
+	 * @author Eric Dunbar
+	 * @date Aug 16, 2016
+	 * @title
+	 * @assignment 2
+	 *
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
